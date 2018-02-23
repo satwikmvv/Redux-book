f7df364259b7bc855d8cdc027719857d13fb2916
satwikmvv <satwikmvv@gmail.com>
2018-02-22 20:44:07 -0500

    initial

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Javascript', pages: 200},
+        { title: 'Lord of the rings', pages: 250},
+        { title: 'The babadook', pages: 450},
+        { title: 'The Hobbit', pages: 500}
+    ]
+}
